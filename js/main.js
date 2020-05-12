3a49b84ba4ce66dcc5a626c69f1ee6b155b38b11
Tai Pham <tai.pham@hdwebsoft.co>
2020-05-12 10:34:17 +0700

    Add anchors to the headers throughout the home page

--- js/main.js
+++ js/main.js
@@ -313,6 +313,18 @@ $('#return-to-top').click(function() {
       scrollTop: 0 // Scroll to top of body
     }, 500);
 });
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 250);
+      return false;
+    }
+  }
+});
 
 var clipboard = new ClipboardJS('.copyToClipboard');
 
